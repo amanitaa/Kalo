7abc5b2c488175e5069259406ecd02130040a6f5
tornike <tornikegerkena02@gmail.com>
2023-07-25 20:31:36 +0400

    added navigation bar

--- app/src/main/java/com/example/kalo/MainActivity.kt
+++ app/src/main/java/com/example/kalo/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.kalo
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var webView: WebView
+    private lateinit var bottomNavigation: BottomNavigationView
+
+    private var baseUrl: String = "https://kalo.ge/"
+
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +23,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         webView = findViewById(R.id.web_view)
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+
         webView.webViewClient = WebViewClient()
-        webView.loadUrl("https://kalo.ge/")
         webView.settings.javaScriptEnabled = true
+        webView.loadUrl(baseUrl)
+
+        bottomNavigation.selectedItemId = R.id.local_mall
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.local_mall -> {
+                    webView.loadUrl(baseUrl)
+                    true
+                }
+                R.id.farmer -> {
+                    webView.loadUrl("$baseUrl/კონსულტაცია/")
+                    true
+                }
+                R.id.info_hub -> {
+                    webView.loadUrl("$baseUrl/my-account/")
+                    true
+                }
+                R.id.agro_news -> {
+                    webView.loadUrl("$baseUrl/news/")
+                    true
+                }
+                R.id.agrometer -> {
+                    webView.loadUrl("$baseUrl/აგრომეტრი/")
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     @Deprecated("Deprecated in Java")
